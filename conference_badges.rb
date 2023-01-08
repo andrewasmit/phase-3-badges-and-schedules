29016c530a01977fd7b7e0aad63cb42adfb980fa
andrew <andrewasmit@hotmail.com>
2023-01-07 19:52:51 -0600

    commit tree 90bd07911dcce07dbafc3c6ebe822e3c722d643d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+    newArr=[]
+    arr.each do |name| 
+       newArr << badge_maker(name)
+    end
+    newArr
+end
+
+def assign_rooms arr
+    newArr = []
+    arr.each_with_index do |name, idx|
+        newArr << "Hello, #{name}! You'll be assigned to room #{idx +1}!"
+    end
+    newArr
+end
+
+def printer arr
+    batch_badge_creator(arr).each {|badge| puts badge}
+    assign_rooms(arr).each {|ass| puts ass} 
+end
